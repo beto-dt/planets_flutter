3fff0f6eca6a2c55d27b7696a9acb38bf46944d0
Luis Alberto De La Torre <lucho.mix34@gmail.com>
2025-03-23 11:37:35 -0500

    feat: configuracion del secure_storage  y solucion del gradler

---
+++ lib/data/local/secure_storage/secure_storage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:planets_flutter/data/local/secure_storage/flutter_secure_storage_provider.dart';
+import 'package:planets_flutter/data/local/secure_storage/isecure_storage.dart';
+
+
+final secureStorageProvider = Provider<ISecureStorage>((ref){
+  final secureStorage = ref.watch(flutterSecureStorageProvider);
+    return SecureStorege(secureStorage);
+});
+
+final class SecureStorege implements ISecureStorage {
+  final FlutterSecureStorage _secureStorage;
+
+  SecureStorege(this._secureStorage);
+  @override
+  Future<void> delete(String key) async {
+    try{
+        await _secureStorage.delete(key: key);
+    } catch(e) {
+        rethrow;
+    }
+  }
+
+  @override
+  Future<String?> read(String key) async {
+    try{
+      await _secureStorage.read(key: key);
+    } catch(e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> write(String key, String value) async {
+    try{
+      await _secureStorage.write(key: key, value: value);
+    } catch(e) {
+      rethrow;
+    }
+  }
+
+}
